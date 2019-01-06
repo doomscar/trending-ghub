c56784e5913ea6907aa4f2e3b5edd492f0806a1f
doomscar <doomscar@gmail.com>
2019-01-06 13:34:05 +0300

    Updates

---
+++ precache-manifest.aff768bcd51d21748d93bf1710a46838.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "281f898aa50f67c44f1b",
+    "url": "/trending-ghub/static/js/runtime~main.281f898a.js"
+  },
+  {
+    "revision": "1502bc93602b73b715bd",
+    "url": "/trending-ghub/static/js/main.1502bc93.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/trending-ghub/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "1502bc93602b73b715bd",
+    "url": "/trending-ghub/static/css/main.0947c4a4.chunk.css"
+  },
+  {
+    "revision": "1074784af4645887eeabdb975a8bce10",
+    "url": "/trending-ghub/index.html"
+  }
+];
